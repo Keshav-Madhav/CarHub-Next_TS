9e246723993aff69354115bc001b5fa348866c89
Keshav Madhav <keshav2552003@gmail.com>
2023-09-24 17:21:18 +0530

    Navbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,9 +3,9 @@
 import { CustombuttonProps } from "@/types"
 import Image from "next/image"
 
-const CustomButton = ({ title, containerStyles, handleClick}: CustombuttonProps) => {
+const CustomButton = ({ title, containerStyles, btnType, handleClick}: CustombuttonProps) => {
   return (
-    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+    <button disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
         <span className={`flex-1`}>
             {title}
         </span>
--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,6 @@ import { MouseEventHandler } from "react";
 export interface CustombuttonProps {
     title: string;
     containerStyles?: string;
+    btnType?: "button" | "submit";
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
