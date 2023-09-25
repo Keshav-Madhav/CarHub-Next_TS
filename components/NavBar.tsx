ae069e32c872f352305330fb8118f45c48eebdc7
Keshav Madhav <keshav2552003@gmail.com>
2023-09-25 13:28:30 +0530

    Footer

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
                 <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain'/>
             </Link>
 
-            <CustomButton title='Sign In' btnType="button" containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'/>
+            <CustomButton title='Sign In' btnType="button" containerStyles='text-primary-blue rounded-full bg-white min-w-[130px] z-10'/>
         </nav>
     </header>
   )
